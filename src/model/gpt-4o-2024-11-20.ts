7342347cd7930714678072cc9696deef257bcda2
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2025-02-27 01:22:05 -0800

    feat: add new models and update pricing

---
+++ src/model/gpt-4o-2024-11-20.ts
@@ -0,0 +1,40 @@
+/* eslint-disable import/extensions */
+import bpeRanks from '../bpeRanks/o200k_base.js'
+import { GptEncoding } from '../GptEncoding.js'
+
+export * from '../constants.js'
+export * from '../specialTokens.js'
+// prettier-ignore
+const api = GptEncoding.getEncodingApiForModel('gpt-4o-2024-11-20', () => bpeRanks)
+const {
+  decode,
+  decodeAsyncGenerator,
+  decodeGenerator,
+  encode,
+  encodeGenerator,
+  isWithinTokenLimit,
+  countTokens,
+  encodeChat,
+  encodeChatGenerator,
+  vocabularySize,
+  setMergeCacheSize,
+  clearMergeCache,
+  estimateCost,
+} = api
+export {
+  clearMergeCache,
+  countTokens,
+  decode,
+  decodeAsyncGenerator,
+  decodeGenerator,
+  encode,
+  encodeChat,
+  encodeChatGenerator,
+  encodeGenerator,
+  estimateCost,
+  isWithinTokenLimit,
+  setMergeCacheSize,
+  vocabularySize,
+}
+// eslint-disable-next-line import/no-default-export
+export default api
---
+++ src/model/o1.ts
@@ -0,0 +1,3 @@
+// eslint-disable-next-line no-restricted-exports, import/no-default-export
+export { default } from '../encoding/o200k_base.js'
+export * from '../encoding/o200k_base.js'
