c015171cc86fda7ed42885b5ddf941bb2f29d727
Bazyli Brzoska <bazyli.brzoska@gmail.com>
2025-10-11 08:54:32 -0700

    docs: add metadata to demo website

--- demo/src/components/layout/HeroSection.tsx
+++ demo/src/components/layout/HeroSection.tsx
@@ -14,6 +14,7 @@ export function HeroSection() {
             width="150"
             height="30"
             frameBorder="0"
+            loading="lazy"
             style={{ backgroundColor: 'transparent' }}
           />
         </div>
